03fd9640058b7af9d0fc9c6f2b943c43e1444256
Chaitanya Chilukuri <135718767+ChaitanyaChilukuri663@users.noreply.github.com>
2024-08-26 23:04:29 +0530

    Added player movement using JSON configuration

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.IO;
+using Newtonsoft.Json.Linq;
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 3f;
+    void Start()
+    {
+        string json = File.ReadAllText(Application.dataPath + "/doofus_diary.json");
+        JObject playerData = JObject.Parse(json);
+        speed = (float)playerData["player_data"]["speed"];
+    }
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
